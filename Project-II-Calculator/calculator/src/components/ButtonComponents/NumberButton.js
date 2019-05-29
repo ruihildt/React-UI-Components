b6bf7c2f26d554d8f048b5ede430633b39840fae
ruihildt <ruihildt@protonmail.com>
2019-05-29 15:23:12 +0200

    Add and import  Calculator Display to App.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+
+function NumberButtons() {
+    return ( 
+        <p></p>
+    );
+}
+
+export default NumberButtons;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Display.css';
+
+
+function CalculatorDisplay() {
+    return ( 
+        <div className="container">
+            <div className="result"></div>
+            <div className="buttons">
+                <div className="left-col"></div>
+                <div className="right-col"></div>
+            </div>
+        </div>
+    );
+}
+
+export default CalculatorDisplay;
